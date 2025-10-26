28118722708e01d09fd0c218e0aa6d3611308067
Aleksandar Lekov <aleksandarlekov11@gmail.com>
2025-10-27 00:24:16 +0100

    backend env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: (import.meta.env['BACKEND_URL'] as string) || 'http://localhost:3000',
+  apiUrl: 'https://conway-game-life-backend.vercel.app',
 };
--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly BACKEND_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
